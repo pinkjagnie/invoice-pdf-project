53e4f6d7553f647a3539547056c7b93a7984111d
pinkjagnie <olga.krzysztofik@gmail.com>
2023-01-25 16:00:23 +0100

    Added styling to Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,21 +2,23 @@ import React from "react";
 
 import About from "./About";
 
+import styles from "./form.module.css";
+
 const Form = () => {
   return(
     <>
       <About />
       <div>
-        <form>
-          <div>
+        <form className={styles.form}>
+          <div className={styles.control}>
             <label htmlFor="name">Your name</label>
             <input type="text" id="name" required />
           </div>
-          <div>
+          <div className={styles.control}>
             <label htmlFor="email">Your email</label>
             <input type="email" id="email" required />
           </div>
-          <div>
+          <div className={styles.control}>
             <label htmlFor="note">Note</label>
             <textarea id="note" rows="4" cols="50" required />
           </div>
