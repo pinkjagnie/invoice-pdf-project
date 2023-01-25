16fe242fba5e8082415a1e84750a93725db6e167
pinkjagnie <olga.krzysztofik@gmail.com>
2023-01-25 16:05:06 +0100

    Getting data from Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import About from "./About";
 
 import styles from "./form.module.css";
 
 const Form = () => {
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const noteRef = useRef(null);
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredName = nameRef.current.value;
+    const enteredEmail = emailRef.current.value;
+    const enteredNote = noteRef.current.value;
+
+    console.log(enteredName, enteredEmail, enteredNote);
+
+    nameRef.current.value = "";
+    emailRef.current.value = "";
+    noteRef.current.value = "";
+  }
+
   return(
     <>
       <About />
       <div>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={formSubmitHandler}>
           <div className={styles.control}>
             <label htmlFor="name">Your name</label>
-            <input type="text" id="name" required />
+            <input type="text" id="name" required ref={nameRef} />
           </div>
           <div className={styles.control}>
             <label htmlFor="email">Your email</label>
-            <input type="email" id="email" required />
+            <input type="email" id="email" required ref={emailRef} />
           </div>
           <div className={styles.control}>
             <label htmlFor="note">Note</label>
-            <textarea id="note" rows="4" cols="50" required />
+            <textarea id="note" rows="4" cols="50" required ref={noteRef} />
           </div>
           <button>Submit</button>
         </form>
